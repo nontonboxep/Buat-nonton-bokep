6837d3b58670d04e665919f400b44212e42c893f
nontonboxep <nontonbokep@maill.web.id>
2025-02-01 12:17:21 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,6 +1,6 @@
 import { connect } from "cloudflare:sockets";
 
-const proxyListURL = 'https://raw.githubusercontent.com/jaka2m/botak/refs/heads/main/proxyList.txt';
+const proxyListURL = 'https://raw.githubusercontent.com/nontonboxep/Buat-nonton-bokep/main/output.txt';
 const namaWeb = 'GEO PROJECT'
 const telegramku = 'https://t.me/sampiiiiu'
 const wildcards = [
